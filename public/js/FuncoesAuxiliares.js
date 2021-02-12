1d0b807dcb447977fe1c4989f062b4a9cc717d73
MarianaKuchida <mariana.kuchida@gmail.com>
2021-02-11 23:03:18 -0300

    validacao de senhas em registre-se, mas ainda sem os tooltips

--- public/js/FuncoesAuxiliares.js
+++ public/js/FuncoesAuxiliares.js
@@ -8,3 +8,16 @@ function mouseoutPass() {
   obj.type = "password";
 }
     
+function Verifica(){
+    val1=document.getElementById("senha").value;
+    val2=document.getElementById("senhaEscolhida").value;
+    if(val1!=val2){
+        document.getElementById("senha").style.borderColor="#f00";
+        document.getElementById("senhaEscolhida").style.borderColor="#f00";
+        alert("As senhas são diferentes");
+    }
+    else{
+        document.getElementById("senha").style.borderColor="#000";
+        document.getElementById("senhaEscolhida").style.borderColor="#000";
+    }
+}
